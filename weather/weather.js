791793800cf076192268952665f560e4a9dd0681
Vnymark <vnymark@gmail.com>
2017-08-20 07:02:35 +0200

    Implemented a simple math formula to convert the temperatures from Fahrenheit to Celsius. Also fixed the result to two decimals.

--- weather/weather.js
+++ weather/weather.js
@@ -8,8 +8,8 @@ var getWeather = (lat, lng, callback) => {
     }, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             callback(undefined, {
-                temperature: body.currently.temperature,
-                apparentTemperature: body.currently.apparentTemperature
+                temperature: ((body.currently.temperature-32)*5/9).toFixed(2),
+                apparentTemperature: ((body.currently.apparentTemperature-32)*5/9).toFixed(2)
             });
         } else {
             callback('Unable to fetch weather.');
