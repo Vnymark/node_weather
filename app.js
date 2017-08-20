fd98b337812c9c7f95875956dc773f4501c2e070
Vnymark <vnymark@gmail.com>
2017-08-20 03:06:38 +0200

    Added user input and made the googlemaps request dynamic (based on the input), instead of a hardcoded address.

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for.',
+            string: true
+        }
+    })
+    .help()
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.a);
+console.log(encodedAddress);
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=10%20mj%C3%B6lnaregatan%20trollh%C3%A4ttan',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
